98b681d12d0f42a5dcf1457ced02c9a3d6bcea1f
Matteu <39852553+MatteuGT@users.noreply.github.com>
2021-05-24 16:43:03 +0800

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ App::getLocale() }}">
+<head>
+    <title>HOME - MatteuSan</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta property="og:title" content="Home - Luwal Sining-Pagganap" />
+    <meta property="og:description" content="The official website of the Luwal Sining-Pagganap" />
+    <meta name="description" content="The official website of the Luwal Sining-Pagganap" />
+    <meta property="og:image" content="https://luwal-sining.xyz/img/ogphoto.png" />
+</head>
+<body>
+
+</body>
+</html>
